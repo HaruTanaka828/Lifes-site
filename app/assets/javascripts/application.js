dd268f2d646d62f9a91335f8e68aeac4f7ddf16d
(GitHHaruTanaka828 <tanaka.lifes@gmail.com>
2020-12-22 02:35:35 +0000

    add

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,3 +29,27 @@ $(document).on('turbolinks:load', function() {
     event.preventDefault();
   });
 });
+
+$('.top-text-title').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('.top-text-title').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
+
+$('.top-text-text').css('visibility','hidden');
+$(window).scroll(function(){
+ var windowHeight = $(window).height(),
+     topWindow = $(window).scrollTop();
+ $('.top-text-text').each(function(){
+  var targetPosition = $(this).offset().top;
+  if(topWindow > targetPosition - windowHeight + 100){
+   $(this).addClass("fadeInDown");
+  }
+ });
+});
